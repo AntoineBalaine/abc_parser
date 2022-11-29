d1baf6e35d1acea71c703ab9262e00e50d652b41
Antoine Balaine <abalaineaccts@gmail.com>
2022-11-28 21:24:04 -0800

    LocatedSpan: Serialize with wrapper type

--- src/serializer.rs
+++ src/serializer.rs
@@ -1,120 +1,37 @@
-use std::fmt::{Debug, Display};
-use std::str;
-
-use std::ops::{Deref, DerefMut};
 extern crate derive_more;
-use derive_more::{Display, From, Into};
 
 use nom::AsBytes;
 use nom_locate::LocatedSpan;
-use serde::ser::SerializeStruct;
-use serde::{Deserialize, Serialize, Serializer};
+use serde::{Deserialize, Serialize};
 
 extern crate serde;
 
-#[derive(Debug, Eq, PartialEq, Serialize, Deserialize, Clone)]
-pub struct Span<'a> {
+#[derive(Serialize, PartialEq, Eq)]
+#[serde(remote = "LocatedSpan")]
+struct RemoteSpanDef<T: AsBytes + Serialize> {
+    #[serde(getter = "LocatedSpan::location_offset")]
     offset: usize,
+    #[serde(getter = "LocatedSpan::location_line")]
     line: u32,
-    fragment: &'a str,
+    #[serde(getter = "LocatedSpan::fragment")]
+    fragment: T,
+    #[serde(getter = "LocatedSpan::get_column")]
     column: usize,
 }
-impl<'a> Span<'a> {
-    pub fn new(input: &'a str) -> Self {
-        Span {
-            offset: 0,
-            line: 1,
-            fragment: input,
-            column: 1,
-        }
+
+// Provide a conversion to construct the remote type.
+impl<T: AsBytes + Serialize> From<RemoteSpanDef<T>> for LocatedSpan<T> {
+    fn from(def: RemoteSpanDef<T>) -> LocatedSpan<T> {
+        LocatedSpan::new(def.fragment)
     }
 }
-impl<'a> From<LocatedSpan<&'a str>> for Span<'a> {
-    fn from(span: LocatedSpan<&'a str>) -> Self {
-        Span {
-            offset: span.location_offset(),
-            line: span.location_line(),
-            fragment: span.fragment(),
-            column: span.get_column(),
-        }
-    }
-}
+
+#[derive(serde::Serialize)]
+struct Span<T: AsBytes + Serialize>(#[serde(with = "RemoteSpanDef")] LocatedSpan<T>);
 
 #[test]
-fn test_new_span() {
-    assert_eq!(
-        Span::new(""),
-        Span {
-            offset: 0,
-            line: 1,
-            fragment: "",
-            column: 1,
-        }
-    );
-}
-#[test]
-fn test_convert_locatedspan() {
-    let input = LocatedSpan::new("Ablabla");
-    let converted_span = Span {
-        offset: input.location_offset(),
-        line: input.location_line(),
-        fragment: input.fragment(),
-        column: input.get_column(),
-    };
-    assert_eq!(Span::from(input), converted_span)
-}
-#[test]
-fn test_serialize_span() {
-    let input = Span::new("");
-    // Convert the Point to a JSON string.
-    let serialized = serde_json::to_string(&input).unwrap();
-    let converted = "{\"offset\":0,\"line\":1,\"fragment\":\"\",\"column\":1}";
-    assert_eq!(serialized, converted)
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, From, Into)]
-struct MySpan<T: AsBytes>(LocatedSpan<T>);
-
-impl<T: AsBytes> Deref for MySpan<T> {
-    type Target = LocatedSpan<T>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-impl<T: AsBytes> DerefMut for MySpan<T> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-
-impl<T: AsBytes + Debug + Display> Serialize for MySpan<T> {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let mut seq = serializer.serialize_struct("MySpan", 4)?;
-        seq.serialize_field("offset", &self.location_offset())?;
-        seq.serialize_field("line", &self.location_line())?;
-        seq.serialize_field(
-            "fragment",
-            &str::from_utf8(self.fragment().as_bytes()).unwrap(),
-        )?;
-        seq.serialize_field("column", &self.get_column())?;
-        seq.end()
-    }
-}
-
-impl<T: AsBytes + Debug> MySpan<T> {
-    fn new(input: T) -> Self {
-        MySpan(LocatedSpan::new(input))
-    }
-}
-
-#[test]
-fn serialize_my_span() {
-    let input = MySpan::new("");
-    // Convert the Point to a JSON string.
+fn serialize_located_span() {
+    let input = Span(LocatedSpan::new(""));
     let serialized = serde_json::to_string(&input).unwrap();
     let converted = "{\"offset\":0,\"line\":1,\"fragment\":\"\",\"column\":1}";
     assert_eq!(serialized, converted)
