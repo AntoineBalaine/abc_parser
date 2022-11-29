537e58dce3c57e1d2713192e5a102aa191724786
Antoine Balaine <abalaineaccts@gmail.com>
2022-11-28 23:24:45 -0800

    Revert "clean-up"
    This reverts commit a4b91e852304d5db8d8c08fe54b7641cd42c5b00.

--- src/abc_parse.rs
+++ src/abc_parse.rs
@@ -6,5 +6,6 @@ struct AbcParser;
 
 pub fn abc_parse(song: String) {
     let tune = AbcParser::parse(Rule::file_structure, &song);
+
     println!("{:?}\n\n", tune.expect("parsing failed"));
 }
--- src/main.rs
+++ src/main.rs
@@ -15,4 +15,9 @@ fn main() {
     let args = Args::parse();
     let content = std::fs::read_to_string(&args.path).expect("could not read file");
     abc_parse::abc_parse(content);
+    /*     let successful_parse = AbcParser::parse(Rule::TEXT, "-273.15");
+    println!("{:?}\n\n", successful_parse.unwrap());
+
+    let unsuccessful_parse = AbcParser::parse(Rule::TEXT, "this is a %");
+    println!("{:?}", unsuccessful_parse); */
 }
